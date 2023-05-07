c1769104dde9ca8edf462acf43da06e19a419dca
Chriss <f710329@gmail.com>
2023-05-07 21:52:53 +0800

    訂餐系統頁面更新

--- prjShanLiang/Controllers/ShoppingController.cs
+++ prjShanLiang/Controllers/ShoppingController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using prjShanLiang.Models;
+using System.Text.Json;
 
 namespace prjShanLiang.Controllers
 {
@@ -15,14 +16,27 @@ namespace prjShanLiang.Controllers
         {
             return View();
         }
-        public IActionResult List()
+        public IActionResult RestaurantMenu()
         {
-            IEnumerable<MealMenu> datas = null;
-            
-                datas = _db.MealMenus;
-            
-           
-            return View(datas);   //強型別放到View
+            IEnumerable<MealMenu> datas = _db.MealMenus;                       
+            return View(datas);   
+        }
+        public IActionResult CartView()
+        {
+            //if (!HttpContext.Session.Keys.Contains(CDictionary.SK_PURCHASED_PRODUCTS_LIST))
+            //    return RedirectToAction("RestaurantMenu");
+            //string json = HttpContext.Session.GetString(CDictionary.SK_PURCHASED_PRODUCTS_LIST);
+
+            //RestaurantMenu<CShoppingCartItem> cart =
+            //    JsonSerializer.Deserialize<RestaurantMenu<CShoppingCartItem>>(json);
+            //if (cart == null)
+            //    return RedirectToAction("RestaurantMenu");
+            //return View(cart);
+            return View();
+        }
+        public IActionResult CheckoutCart()
+        {
+            return View();
         }
     }
 }
--- prjShanLiang/Program.cs
+++ prjShanLiang/Program.cs
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Shopping}/{action=List}/{id?}");
+    pattern: "{controller=Shopping}/{action=RestaurantMenu}/{id?}");
 
 app.Run();
