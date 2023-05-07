39dd75e9ac5b09e710ddfc270e8978a10369f6f6
Chriss <f710329@gmail.com>
2023-05-07 14:13:45 +0800

    訂餐畫面第一版

---
+++ prjShanLiang/Controllers/ShoppingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjShanLiang.Models;
+
+namespace prjShanLiang.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        private readonly ShanLiang21Context _db;
+
+        public ShoppingController(ShanLiang21Context db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            IEnumerable<MealMenu> datas = null;
+            
+                datas = _db.MealMenus;
+            
+           
+            return View(datas);   //強型別放到View
+        }
+    }
+}
--- prjShanLiang/Program.cs
+++ prjShanLiang/Program.cs
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Shopping}/{action=List}/{id?}");
 
 app.Run();
