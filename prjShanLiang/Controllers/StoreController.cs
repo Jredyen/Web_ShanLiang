e67dafd43dedd01ec3f18f6a77e0e640f884b37f
Jredyen <wii88051@gmail.com>
2023-05-08 20:10:32 +0800

    調整排版、改進搜尋、刪除搜尋時不必要的資料

--- prjShanLiang/Controllers/StoreController.cs
+++ prjShanLiang/Controllers/StoreController.cs
@@ -30,9 +30,9 @@ namespace prjShanLiang.Controllers
         {
             if (id == null)
                 return RedirectToAction("Reconnend");
-            IQueryable datas = from s in _db.Stores 
-                        where s.StoreId == id 
-                        select s;
+            IQueryable datas = from s in _db.Stores
+                               where s.StoreId == id
+                               select s;
             if (datas == null)
                 return RedirectToAction("Reconnend");
             return View(datas);
@@ -47,9 +47,31 @@ namespace prjShanLiang.Controllers
             IQueryable datas = _db.RestaurantTypes.Select(r => r.TypeName);
             return Json(datas);
         }
-        public IActionResult SerachStore(string keyword)
+        public IActionResult SerachStore(string keyword, int[] type, int[] district)
         {
-            IQueryable storeList = _db.Stores.Where(s => s.RestaurantName.Contains(keyword)).Select(n => n);
+            IQueryable storeList = _db.Stores.Where(s => s.RestaurantName.Contains(keyword)).Select(s => new
+            {
+                //TODO:整合店名、類型與地區的搜尋
+                //店名搜尋 : 完成
+                //類型搜尋 : 未完成
+                //地區搜尋 : 未完成
+                s.RestaurantName,
+                s.StoreId,
+                s.Rating,
+                s.OpeningTime,
+                s.ClosingTime,
+                s.RestaurantPhone,
+                s.RestaurantAddress,
+                imagePath = _db.StoreDecorationImages
+                    .Where(x => x.StoreId == s.StoreId)
+                    .Select(x => x.ImagePath).ToList(),
+                typeName = _db.StoreTypes
+                    .Where(st => st.StoreId == s.StoreId)
+                    .Join(_db.RestaurantTypes,
+            st => st.RestaurantTypeNum,
+            rt => rt.RestaurantTypeNum,
+            (st, rt) => rt.TypeName).ToList(),
+            });
             return Json(storeList);
         }
     }
---
+++ prjShanLiang/Models/StoreAdImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prjShanLiang.Models;
+
+public partial class StoreAdImage
+{
+    public int ImageNo { get; set; }
+
+    public int? StoreId { get; set; }
+
+    public string? Adimage { get; set; }
+
+    public virtual Store? Store { get; set; }
+}
