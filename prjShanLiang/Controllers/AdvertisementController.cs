640bea349788f429b61f35d505ee4a2d4637a86a
coco <coco@gmail.com>
2023-05-09 17:35:58 +0800

    殘念的首頁廣告圖片 左圖右圖沒辦法改變

--- prjShanLiang/Controllers/AdvertisementController.cs
+++ prjShanLiang/Controllers/AdvertisementController.cs
@@ -19,73 +19,13 @@ namespace prjShanLiang.Controllers
         {
 
 
-            var Image = _db.Stores.Take(2).ToList();
+            var stores = _db.Stores.Take(2).ToList();
             var AdImages = _db.StoreAdImages.ToList();
-            var model = new Tuple<List<Store>, List<StoreAdImage>>(Image, AdImages);
+            var model = new Tuple<List<Store>, List<StoreAdImage>>(stores, AdImages);
             return View(model);
            
         }
-        public IActionResult AddToAdvCart(int? id)
-        {
-            if (id == null)
-                return RedirectToAction("BuyAdv");
-            ViewBag.MealId = id;
-            return View();
-        }
-        [HttpPost]
-        public ActionResult AddToAdvCart(CAddToCartViewModel vm)
-        {
-            MealMenu menu = _db.MealMenus.FirstOrDefault(t => t.MealId == vm.txtMealId);
-            if (menu != null)
-            {
-                string json = "";
-                List<CShoppingCartItem> cart = null;
-                if (HttpContext.Session.Keys.Contains(CDictionary.SK_PURCHASED_MENU_LIST))
-                { //如果比對有KEY就把json的值取出 cart還原回來
-                    json = HttpContext.Session.GetString(CDictionary.SK_PURCHASED_MENU_LIST);
-                    cart = JsonSerializer.Deserialize<List<CShoppingCartItem>>(json);
-                }
-                else
-                {
-                    cart = new List<CShoppingCartItem>();
-                }
-                bool verify = false;
-                foreach (var shoppingItem in cart)
-                {
-                    if (shoppingItem.mealId == menu.MealId)
-                    {
-                        shoppingItem.count += vm.txtCount;
-                        verify = true;
-                        break;
-                    }
-                }
-                if (!verify)
-                {
-                    CShoppingCartItem item = new CShoppingCartItem();
-                    item.mealmenu = menu;
-                    item.price = (decimal)menu.MealPrice;
-                    item.mealId = vm.txtMealId;
-                    item.count = vm.txtCount;
-                    cart.Add(item);
-                }
-
-                json = JsonSerializer.Serialize(cart);  //購物車資料變回json
-                HttpContext.Session.SetString(CDictionary.SK_PURCHASED_MENU_LIST, json);
-            }
-            return RedirectToAction("BuyAdv");
-        }
-        public IActionResult CartView()
-        {
-            if (!HttpContext.Session.Keys.Contains(CDictionary.SK_PURCHASED_MENU_LIST))
-                return RedirectToAction("BuyAdv");
-            string json = HttpContext.Session.GetString(CDictionary.SK_PURCHASED_MENU_LIST);
-
-            List<CShoppingCartItem> cart = JsonSerializer.Deserialize<List<CShoppingCartItem>>(json);
-            if (cart == null)
-                return RedirectToAction("BuyAdv");
-            return View(cart);
-        }
-
+      
 
 
     }
