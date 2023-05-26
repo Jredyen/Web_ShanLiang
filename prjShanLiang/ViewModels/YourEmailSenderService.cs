c4b9ce3fdce2cc306633c746170beb18152fd01a
dark <dark@ispan.com.tw>
2023-05-26 17:35:18 +0800

    更新

--- prjShanLiang/ViewModels/YourEmailSenderService.cs
+++ prjShanLiang/ViewModels/YourEmailSenderService.cs
@@ -19,13 +19,13 @@
 
         public Task SendEmailAsync(string? email, string subject, string message, int? id)
         {
-            string senderEmail = _configuration["EmailSender:Email"]; // 发件人邮箱
-            string senderPassword = _configuration["EmailSender:Password"]; // 发件人邮箱密码
+            string senderEmail = _configuration["EmailSender:Email"]; // 發件人郵箱
+            string senderPassword = _configuration["EmailSender:Password"]; // 發件人郵箱密碼
 
-            // 设置发送邮件的配置
+            // 設置發送郵件的配置
             var smtpClient = new SmtpClient
             {
-                Host = "smtp.gmail.com", // Gmail SMTP服务器
+                Host = "smtp.gmail.com", // Gmail SMTP服務器
                 Port = 587,
                 EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
@@ -33,37 +33,37 @@
                 Credentials = new NetworkCredential(senderEmail, senderPassword)
             };
 
-            // 创建邮件对象
+            // 創建郵件對象
             var mailMessage = new MailMessage(senderEmail, email)
             {
                 Subject = subject,
                 IsBodyHtml = true
             };
 
-            // 获取当前请求的主机名和端口号
+            // 獲取當前請求的主機名稱和端口號
             var request = _httpContextAccessor.HttpContext.Request;
             string host = request.Host.Host;
             int port = (int)request.Host.Port;
 
-            // 构建邮件正文，包括验证链接
-            string verificationLink = $"https://{host}:{port}/StoreAdmin/CompleteVerification/?id={id}"; // 根据传入的 id 构建验证链接
+            // 構建郵件正文，包括驗證鏈接
+            string verificationLink = $"https://{host}:{port}/StoreAdmin/CompleteVerification/?id={id}"; // 根據傳入的id構建驗證鏈接
             string emailBody = $"<p>{message}</p>";
-            emailBody += $"<p>请点击以下链接进行验证：</p>";
-            emailBody += $"<p><a href=\"{verificationLink}\">驗證連結</a></p>";
+            emailBody += $"<p>請點擊以下鏈接進行驗證：</p>";
+            emailBody += $"<p><a href=\"{verificationLink}\">驗證鏈結</a></p>";
 
             mailMessage.Body = emailBody;
 
             try
             {
-                // 发送邮件
+                // 發送郵件
                 smtpClient.Send(mailMessage);
                 return Task.CompletedTask;
             }
             catch (Exception ex)
             {
-                // 处理发送邮件时的异常
-                // 可以添加日志记录或其他错误处理逻辑
-                throw new ApplicationException("邮件发送失败: " + ex.Message);
+                // 處理發送郵件時的異常
+                // 可以添加日誌紀錄或其他錯誤處理邏輯
+                throw new ApplicationException("郵件發送失敗:" + ex.Message);
             }
         }
     }
