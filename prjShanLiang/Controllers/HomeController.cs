fd69562ec085ae180dbb8d2a118f18008a285102
coco <coco@gmail.com>
2023-05-26 14:53:43 +0800

    首頁收藏排序搞定

--- prjShanLiang/Controllers/HomeController.cs
+++ prjShanLiang/Controllers/HomeController.cs
@@ -17,16 +17,20 @@ namespace prjShanLiang.Controllers
        
             public IActionResult Index()
             {
+
             var Image = _db.Stores.ToList();
             var AdImages = _db.StoreAdImages.Where(sa => sa.ADColumn == "true").ToList();
             var Evaluate = _db.StoreEvaluates.ToList();
             var member = _db.Members.ToList();
             var blog = _db.Blogs.ToList();
-            var model = new Tuple<List<Store>, List<StoreAdImage>,List<StoreEvaluate>,List<Member>,List<Blog>>(Image, AdImages, Evaluate, member,blog);
+            var MemAction = _db.MemberActions.Where(Ma=>Ma.ActionId==2).ToList();
+            var model = new Tuple<List<Store>, List<StoreAdImage>,List<StoreEvaluate>,List<Member>,List<Blog>,List<MemberAction>>(Image, AdImages, Evaluate, member,blog, MemAction);
             return View(model);
-            }
-        
 
+
+            }
+
+     
         public IActionResult Privacy()
         {
             return View();
