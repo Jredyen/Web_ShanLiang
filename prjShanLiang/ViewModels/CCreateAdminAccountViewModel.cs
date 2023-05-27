3d04088fbfa7771240689ce183431553718f1f71
dark <dark@ispan.com.tw>
2023-05-27 14:39:02 +0800

    更新

---
+++ prjShanLiang/ViewModels/CCreateAdminAccountViewModel.cs
@@ -0,0 +1,19 @@
+﻿using prjShanLiang.Models;
+
+namespace prjShanLiang.ViewModels
+{
+    public class CCreateAdminAccountViewModel
+    {
+        public Admin Admin { get; set; }
+
+        public int AdminId { get; set; }
+
+        public string? AdminName { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Password2 { get; set; }
+
+        public int? IdentificationId { get; set; }
+    }
+}
